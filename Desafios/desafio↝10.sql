971c6d24c01d0d393c9e7ea17809cb5d42fc4a1c
dougfunny <douglaseduardo220483@gmail.com>
2020-02-21 16:42:59 -0300

    DESAFIO 10

---
+++ Desafios/desafio↝10.sql
@@ -0,0 +1,6 @@
+# Mostre todos os dados em ordem decrescente ordenados 
+# por created_by onde created_by é maior ou igual a 3.
+
+SELECT * FROM northwind.purchase_orders
+	WHERE created_by >= 3
+	ORDER BY created_by DESC;
