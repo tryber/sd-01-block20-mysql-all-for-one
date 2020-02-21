6c68876131ea9efa53754d8c4c02e2afd6f0ea85
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-21 16:03:27 -0300

    subindo todos os arquivos, com excecao do 19

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+CREATE TABLE northwind.Antonio (
+	`A` int,
+    `Trybe` varchar(255),
+    `eh` int
+);
+
+INSERT INTO northwind.Antonio VALUES ((5+6), 'de', (2+8));
+SELECT * FROM northwind.Antonio;
+
+
