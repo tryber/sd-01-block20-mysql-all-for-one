c383cb53693f41ff97b85849cd7702cc69db47c2
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-21 17:09:46 -0300

    codeCLimate

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,7 @@
 CREATE TABLE northwind.Antonio (
-	`A` int,
-    `Trybe` varchar(255),
-    `eh` int
+  `A` int,
+  `Trybe` varchar(255),
+  `eh` int
 );
 
 INSERT INTO northwind.Antonio VALUES ((5+6), 'de', (2+8));
-
-
