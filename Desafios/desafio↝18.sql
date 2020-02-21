a7d85741b3fb826939eff3e26ba7e03ecdc93e2d
dougfunny <douglaseduardo220483@gmail.com>
2020-02-21 18:06:56 -0300

    PULANDO O 18 E O 19. FALTANDO INFORMAÇÕES

---
+++ Desafios/desafio↝18.sql
@@ -0,0 +1,1 @@
+# Mostre quais dados de purchase_orders tem suplier_id igual a 3 e type igual a done.
---
+++ Desafios/desafio↝19.sql
@@ -0,0 +1,2 @@
+# Descubra quantos pedidos foram feitos onde os pedidos foram feitos pelo empregador,
+# com o EmployeeID 6 ou 5 e que tenham sidos enviados via o método 2.
