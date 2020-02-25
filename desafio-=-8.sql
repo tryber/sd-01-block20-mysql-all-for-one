a0c300f7e2ddc35e7eedc854f7cccd3b8b7bf598
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-02-25 12:05:26 -0300

    exercise 5 as 10

---
+++ desafio-=-8.sql
@@ -0,0 +1,10 @@
+USE `northwind`;
+
+CREATE TABLE IF NOT EXISTS Exercises_8 (
+    A INT(20),
+    TRYBE VARCHAR(255),
+    EH INT(20)
+);
+
+INSERT INTO `Exercises_8` (A, TRYBE, EH)
+	VALUES (5+6, "DE", 2+8);
