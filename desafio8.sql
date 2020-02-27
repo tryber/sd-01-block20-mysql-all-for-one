a101114b5d98c645ae94f35e0512a8d8dd6c0beb
Mateus Talles <mateustalles@gmail.com>
2020-02-26 23:00:09 -0300

    Projeto finalizado.

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+CREATE TABLE northwind.tabela (
+  A INT NOT NULL,
+  Trybe VARCHAR(45) NOT NULL,
+  eh INT NOT NULL,
+  PRIMARY KEY (A));
+INSERT INTO northwind.tabela (A, Trybe, eh) VALUES
+	(5+6, 'de', 2+8);
