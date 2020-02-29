642b8a70e59afac0d3dbfc57bb8169af658641c9
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-29 14:28:33 -0300

    desafio 21

---
+++ desafio21.sql
@@ -0,0 +1,8 @@
+/* Adicione duas linhas ao order_details com os mesmos dados. Esses dados são novamente order_id: 69,
+product_id: 80, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL,
+purchase_order_id: NULL e inventory_id: 129 (o Id deve ser incrementado automaticamente). */
+
+INSERT INTO northwind.order_details
+(order_id, product_id, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+VALUE (69, 80, 15.0000, 0, 2, NULL, NULL, 129),
+(69, 80, 15.0000, 0, 2, NULL, NULL, 129);
